9fc8b8ff9171a11735dfbff806ab06138b5fbf99
NdekoCode <arickbulakali@gmail.com>
2023-06-19 15:00:41 +0200

    refactor: add assets structure

--- src/app/shared/components/input-container/input-container.component.ts
+++ src/app/shared/components/input-container/input-container.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-input-container',
-  templateUrl: './input-container.component.html',
-  styleUrls: ['./input-container.component.scss']
+  templateUrl: './input-container.component.html'
 })
 export class InputContainerComponent {
   label: string = ''
