cd98aab93ca1089f1110b6fbee30073313f565c0
NdekoCode <arickbulakali@gmail.com>
2023-07-07 13:23:32 +0200

    fix: tailwind prefix

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,6 @@ module.exports = {
     styled: false, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
     rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
-    prefix: 'tw' // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    prefix: 'tw-' // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
   }
 }
