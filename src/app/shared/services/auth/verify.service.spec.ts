2f785e24f6bfbd3755020e201031d977c7ee2510
NdekoCode <arickbulakali@gmail.com>
2023-07-07 12:11:56 +0200

    style: add daisyui

--- src/app/shared/services/auth/verify.service.spec.ts
+++ src/app/shared/services/auth/verify.service.spec.ts
@@ -1,23 +1,31 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing'
+import { TestBed } from '@angular/core/testing'
+import { environment } from 'src/environments/environment'
+import { ApiConfigService } from '../api-config/api-config.service'
+import { VerifyService } from './verify.service'
+
 xdescribe('VerifyService', () => {
-  // let service: VerifyService
+  let service: VerifyService
 
-  // let httpMock: HttpTestingController
-  // const apiConfigSpy = jest.spyOn(ApiConfigService.prototype, 'url', 'get')
-  // apiConfigSpy.mockReturnValue(environment.BASE_URL)
-
-  // beforeEach(() => {
-  // TestBed.configureTestingModule({
-  // imports: [HttpClientTestingModule]
-  // providers: [VerifyService, { useValue: { url: environment.BASE_URL } }]
-  // })
-  // service = TestBed.inject(VerifyService)
-  // httpMock = TestBed.inject(HttpTestingController)
-  // })
+  let httpMock: HttpTestingController
+  const apiConfigSpy = jest.spyOn(ApiConfigService.prototype, 'url', 'get')
+  apiConfigSpy.mockReturnValue(environment.BASE_URL)
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [VerifyService, { useValue: { url: environment.BASE_URL } }]
+    })
+    service = TestBed.inject(VerifyService)
+    httpMock = TestBed.inject(HttpTestingController)
+  })
 
   xit('should be created', () => {
     // expect(service).toBeTruthy()
   })
-  /*
+
   xit('should send a GET request to verify user email', () => {
     const token = 'testToken'
     const expectedResponse = { message: 'Email verified successfully' }
@@ -51,5 +59,5 @@ xdescribe('VerifyService', () => {
   })
   afterEach(() => {
     httpMock.verify()
-  }) */
+  })
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,5 +25,13 @@ module.exports = {
       }
     }
   },
-  plugins: []
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: 'dark', // name of one of the included themes for dark mode
+    styled: false, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+    prefix: 'tw' // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+  }
 }
