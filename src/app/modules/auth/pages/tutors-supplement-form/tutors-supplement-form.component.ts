cd3ba9d88bdb0999ff1bed38d7c419227397dcbb
NdekoCode <arickbulakali@gmail.com>
2023-07-11 19:03:33 +0200

    refactor: modify alert class

--- src/app/modules/auth/pages/tutors-supplement-form/tutors-supplement-form.component.ts
+++ src/app/modules/auth/pages/tutors-supplement-form/tutors-supplement-form.component.ts
@@ -34,73 +34,14 @@ export class TutorsSupplementFormComponent implements OnInit {
       photo: ['', Validators.required], // Champ pour la photo de profil
       biography: ['', Validators.required],
       hourlyRate: [0, Validators.required],
-      // Step 2: Informations professionnelles
-      experienceYear: ['', Validators.required], // Champ pour l'année d'expérience
-      educationLevel: ['', Validators.required], // Champ pour le niveau d'études
-      educationDomain: ['', Validators.required], // Champ pour le domaine d'études
-      timePeriod: [''], // Champ pour la période de temps
-      currentlyEnrolled: [false], // Champ pour indiquer si l'utilisateur est actuellement inscrit
-      sinceMonth: [''], // Champ pour le mois de début d'inscription
-      sinceYear: [''], // Champ pour l'année de début d'inscription
-
-      // Step 3: Disponibilité
-      timezone: ['', Validators.required], // Champ pour la timezone
-      days: this.formBuilder.group({
-        monday: this.formBuilder.group({
-          active: [false], // Checkbox pour activer ou désactiver le jour
-          start: [''], // Champ pour l'heure de début de disponibilité
-          end: [''] // Champ pour l'heure de fin de disponibilité
-        }),
-        tuesday: this.formBuilder.group({
-          active: [false],
-          start: [''],
-          end: ['']
-        }),
-        // Ajoutez les autres jours de la semaine de la même manière
-        wednesday: this.formBuilder.group({
-          active: [false],
-          start: [''],
-          end: ['']
-        }),
-        thursday: this.formBuilder.group({
-          active: [false],
-          start: [''],
-          end: ['']
-        }),
-        friday: this.formBuilder.group({
-          active: [false],
-          start: [''],
-          end: ['']
-        }),
-        saturday: this.formBuilder.group({
-          active: [false],
-          start: [''],
-          end: ['']
-        }),
-        sunday: this.formBuilder.group({
-          active: [false],
-          start: [''],
-          end: ['']
-        })
-      }),
 
       // Step 4: Préférences de tutorat
       // Ajoutez les champs pour les préférences de tutorat
       subject: ['', Validators.required], // Champ pour le sujet de tutorat
-      schoolLevel: ['', Validators.required], // Champ pour le niveau scolaire
       tutoringType: ['', Validators.required], // Champ pour le type de tutorat (en ligne ou en présentiel)
-      studentType: ['', Validators.required], // Champ pour le type d'élève (âge
-      // ...
-
-      // Step 4: Préférences de tutorat
-      // Step 5: Compétences en communication
-      writtenCommunication: ['', Validators.required], // Champ pour les compétences en communication écrite
-      oralCommunication: ['', Validators.required], // Champ pour les compétences en communication orale
-      problemSolving: ['', Validators.required], // Champ pour les compétences en résolution de problèmes
-      decisionMaking: ['', Validators.required], // Champ pour les compétences en prise de décision
 
       // Step 6: Antécédents judiciaires
-      backgroundCheck: ['', Validators.required] // Champ pour la vérification des antécédents judiciaires
+      documentCheck: ['', Validators.required] // Champ pour la vérification des antécédents judiciaires
     })
   }
 
