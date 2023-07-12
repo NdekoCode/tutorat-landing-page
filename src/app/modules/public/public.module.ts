9869f3f0a439f57d07deab8cdcfbad11e5e2ad9a
NdekoCode <arickbulakali@gmail.com>
2023-07-12 14:47:45 +0200

    add hero

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -2,7 +2,9 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
 import { FooterComponent } from './components/footer/footer.component'
+import { HeaderComponent } from './components/header/header.component'
 import { MenuItemComponent } from './components/menu-item/menu-item.component'
+import { NotificationBarComponent } from './components/notification-bar/notification-bar.component'
 import { AboutComponent } from './pages/about/about.component'
 import { BlogComponent } from './pages/blog/blog.component'
 import { ContactComponent } from './pages/contact/contact.component'
@@ -20,7 +22,9 @@ import { PublicRoutingModule } from './public-routing.module'
     PublicLayoutComponent,
     BlogComponent,
     MenuItemComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    NotificationBarComponent
   ],
   imports: [CommonModule, PublicRoutingModule]
 })
