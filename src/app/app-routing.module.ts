f1e20a27494a61823b82c92c6399e084b2cb73d1
NdekoCode <arickbulakali@gmail.com>
2023-07-12 09:32:29 +0200

    fix: auth routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,8 +15,7 @@ const routes: Routes = [
     path: 'auth',
     // Dans la memoire ce module sera charger lorsqu'il est demander et uniquement lorsqu'il est demander, alors une fois que l'on va demander cette module il va se charger et seulement quand il sera charger que son routing va se mettre en place
     loadChildren: () =>
-      import('./modules/auth/auth.module').then((m) => m.AuthModule),
-    data: { preload: true }
+      import('./modules/auth/auth.module').then((m) => m.AuthModule)
   },
   {
     path: 'myspace',
