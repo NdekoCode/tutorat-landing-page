98ba82a7a51eafcb3750a9ed7e703d3b4ac60b58
NdekoCode <arickbulakali@gmail.com>
2023-07-18 16:14:34 +0200

    refactor: change module structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,26 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
-import { ErrorComponent } from './components/error/error.component'
 import { AuthGuard } from './core/guards/auth.guard'
+import { ErrorComponent } from './shared/components/error/error.component'
 
 const routes: Routes = [
   {
     path: '',
     // Dans la memoire ce module sera charger lorsqu'il est demander et uniquement lorsqu'il est demander, alors une fois que l'on va demander cette module il va se charger et seulement quand il sera charger que son routing va se mettre en place
     loadChildren: () =>
-      import('./core/utilities/modules/public/public.module').then(
-        (m) => m.PublicModule
-      ),
+      import('./modules/public/public.module').then((m) => m.PublicModule),
     data: { preload: true }
   },
   {
     path: 'auth',
     // Dans la memoire ce module sera charger lorsqu'il est demander et uniquement lorsqu'il est demander, alors une fois que l'on va demander cette module il va se charger et seulement quand il sera charger que son routing va se mettre en place
     loadChildren: () =>
-      import('./core/utilities/modules/auth/auth.module').then(
-        (m) => m.AuthModule
-      )
+      import('./modules/auth/auth.module').then((m) => m.AuthModule)
   },
   {
     path: 'myspace',
     loadChildren: () =>
-      import('./core/utilities/modules/myspace/myspace.module').then(
-        (m) => m.MySpaceModule
-      ),
+      import('./modules/myspace/myspace.module').then((m) => m.MySpaceModule),
     canActivate: [AuthGuard]
   },
   {
