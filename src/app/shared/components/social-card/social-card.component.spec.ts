1e9d27ed19a2f8a6a59010e16a45e71e97ab4429
NdekoCode <arickbulakali@gmail.com>
2023-07-19 23:41:39 +0200

    test: tutors bloc test in my space

--- src/app/shared/components/social-card/social-card.component.spec.ts
+++ src/app/shared/components/social-card/social-card.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-
+import { FormsModule } from '@angular/forms'
 import { SocialCardComponent } from './social-card.component'
 
 describe('SocialCardComponent', () => {
@@ -8,11 +8,44 @@ describe('SocialCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SocialCardComponent]
+      declarations: [SocialCardComponent],
+      imports: [FormsModule]
     }).compileComponents()
 
     fixture = TestBed.createComponent(SocialCardComponent)
     component = fixture.componentInstance
+    component.tutor = {
+      id: 1,
+      firstName: 'Arick',
+      lastName: 'Bul',
+      age: 30,
+      gender: 'male',
+      email: 'arick.bul@zabibu.co',
+      phone: '1234567890',
+      image: 'image.jpg',
+      address: {
+        address: '123 Street',
+        city: 'City',
+        postalCode: '12345',
+        state: 'State'
+      },
+      tutor: {
+        bio: 'Bio',
+        hourlyRate: 50,
+        transport: 1,
+        specialization: ['Math', 'Science'],
+        video: {
+          url: 'video-url'
+        },
+        address: {
+          address: '123 Street',
+          city: 'City',
+          postalCode: '12345',
+          state: 'State'
+        }
+      }
+    }
+    component.usersComment = []
     fixture.detectChanges()
   })
 
