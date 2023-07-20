0c93bd3144e87898afa0c5d9c5000c98f9c6ac3d
NdekoCode <arickbulakali@gmail.com>
2023-07-20 12:28:33 +0200

    style: change shadow expended

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,14 @@ import { ErrorComponent } from './shared/components/error/error.component'
 const routes: Routes = [
   {
     path: '',
-    // Dans la memoire ce module sera charger lorsqu'il est demander et uniquement lorsqu'il est demander, alors une fois que l'on va demander cette module il va se charger et seulement quand il sera charger que son routing va se mettre en place
+    // Dans la memoire ce module sera charger lorsqu'il est demander et uniquement lorsqu'il est demander, alors une fois que l'on va demander ce  module il va se charger et seulement quand il sera charger que son routing va se mettre en place
     loadChildren: () =>
       import('./modules/public/public.module').then((m) => m.PublicModule),
     data: { preload: true }
   },
   {
     path: 'auth',
-    // Dans la memoire ce module sera charger lorsqu'il est demander et uniquement lorsqu'il est demander, alors une fois que l'on va demander cette module il va se charger et seulement quand il sera charger que son routing va se mettre en place
+    // Dans la memoire ce module sera charger lorsqu'il est demander et uniquement lorsqu'il est demander, alors une fois que l'on va demander ce module il va se charger et seulement quand il sera charger que son routing va se mettre en place
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule)
   },
