c9b06f8d232e7143db405b641e306961e0ae1282
NdekoCode <arickbulakali@gmail.com>
2023-07-21 11:36:21 +0200

    feat: get tutor true data

--- src/app/shared/services/tutor/tutor.service.ts
+++ src/app/shared/services/tutor/tutor.service.ts
@@ -34,4 +34,11 @@ export class TutorService {
   isTutor(user: User | ITutor): boolean {
     return isExists(user?.tutor) && !isEmptyObject(user?.tutor)
   }
+  getSingleTutor(id: number): ITutor | undefined {
+    return this.getTutors().find((tutor) => tutor.id === id)
+  }
+  getSuggestionTutors(city: string) {
+    this._tutors = this.getTutors().filter((t) => t.address.city === city)
+    return this._tutors
+  }
 }
