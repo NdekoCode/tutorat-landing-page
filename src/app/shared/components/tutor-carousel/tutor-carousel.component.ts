0b29faa2bb13292b346f9832d0d0c8ec617c0382
NdekoCode <arickbulakali@gmail.com>
2023-07-30 02:04:22 +0200

    fix: carousel item width

--- src/app/shared/components/tutor-carousel/tutor-carousel.component.ts
+++ src/app/shared/components/tutor-carousel/tutor-carousel.component.ts
@@ -17,17 +17,30 @@ export class TutorCarouselComponent implements OnInit, OnDestroy {
   slideWidth!: string
   carouselStyle!: { [key: string]: string }
   widthCarousel!: { width: string }
-
+  containerWidth!: number | string
   ngOnInit(): void {
-    const calculatedSlideWidth = 100 / this.slidesPerPage
-    this.slideWidth = `${calculatedSlideWidth}%`
+    // const calculatedSlideWidth = 100 / this.slidesPerPage
+    // this.slideWidth = `${calculatedSlideWidth}%`
+    this.setStyle()
     this.updateCarouselStyle()
-    this.widthCarousel = { width: `calc(100% / ${this.slidesPerPage})` }
+    // this.widthCarousel = { width: `calc(100% / ${this.slidesPerPage})` }
     this.timer = window.setInterval(() => {
       this.nextSlide()
     }, GLOBAL_CONSTANTS.SLIDE_TIMEOUT)
   }
-
+  setStyle() {
+    // On definis une ratio qui va etre un peu la largeur en pourcentages des elements dans le carousel qui est le nombre d'item diviser par les nombre d'element que l'on veut afficher
+    const ratio = this.items.length / this.slidesPerPage
+    this.slideWidth = `${100 / this.slidesPerPage / ratio}%`
+    this.updateCarouselStyle()
+    this.widthCarousel = { width: `calc(100% / ${this.slidesPerPage / ratio})` }
+    // On definis la largeur du `carousel__container`
+    // On ajoute une largeur calculer avec "ratio"
+    this.containerWidth = `calc(100% * ${
+      (ratio * this.slidesPerPage) / ratio
+    } )`
+    // On parcour tous les elements dans notre `.carousel_container` pour leurs appliquer une largeur calculer
+  }
   prevSlide(): void {
     if (this.currentIndex > 0) {
       this.currentIndex--
@@ -47,7 +60,8 @@ export class TutorCarouselComponent implements OnInit, OnDestroy {
   updateCarouselStyle(): void {
     const translateXValue = this.currentIndex * -parseInt(this.slideWidth, 10)
     this.carouselStyle = {
-      transform: `translateX(${translateXValue}%)`
+      transform: `translateX(${translateXValue}%)`,
+      width: this.containerWidth as string
     }
   }
   ngOnDestroy(): void {
