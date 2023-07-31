7e23c2107d261b5bc0e5644619bbb06957c37da1
NdekoCode <arickbulakali@gmail.com>
2023-07-31 12:55:23 +0200

    test: logout page

--- src/app/modules/auth/pages/logout/logout.component.spec.ts
+++ src/app/modules/auth/pages/logout/logout.component.spec.ts
@@ -1,5 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
+import {
+  GoogleLoginProvider,
+  SocialAuthServiceConfig
+} from '@abacritt/angularx-social-login'
+import { HttpClientModule } from '@angular/common/http'
+import { AngularFireModule } from '@angular/fire/compat'
+import { AngularFireStorageModule } from '@angular/fire/compat/storage'
+import { ApiConfigService } from 'src/app/shared/services/api-config/api-config.service'
+import { AuthService } from 'src/app/shared/services/auth/auth.service'
+import { environment } from 'src/environments/environment'
 import { LogoutComponent } from './logout.component'
 
 describe('LogoutComponent', () => {
@@ -8,7 +18,28 @@ describe('LogoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LogoutComponent]
+      declarations: [LogoutComponent],
+      imports: [
+        HttpClientModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireStorageModule
+      ],
+      providers: [
+        ApiConfigService,
+        AuthService,
+        {
+          provide: 'SocialAuthServiceConfig',
+          useValue: {
+            autoLogin: false,
+            providers: [
+              {
+                id: GoogleLoginProvider.PROVIDER_ID,
+                provider: new GoogleLoginProvider('')
+              }
+            ]
+          } as SocialAuthServiceConfig
+        }
+      ]
     }).compileComponents()
 
     fixture = TestBed.createComponent(LogoutComponent)
--- src/app/modules/myspace/myspace-layout.component.ts
+++ src/app/modules/myspace/myspace-layout.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core'
-import { Observable } from 'rxjs'
 import { MYSPACE_ROUTES } from 'src/app/core/routes/routes'
-import { User } from 'src/app/core/utilities/types'
-import { UserService } from 'src/app/shared/services/user/user.service'
 
 @Component({
   selector: 'app-myspace-layout',
@@ -15,8 +12,6 @@ export class MyspaceLayoutComponent {
     MYSPACE_ROUTES.MESSAGES,
     MYSPACE_ROUTES.CALENDAR
   ]
-  user: Observable<User> = this.userService.getUserAuth() as Observable<User>
-  constructor(private userService: UserService) {}
   toggleActive() {
     this.isActive = !this.isActive
   }
